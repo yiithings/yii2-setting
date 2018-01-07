cddda9e59f59c8d45696dd7b777e7455dc6e96c2
panlatent <panlatent@gmail.com>
2018-01-08 03:59:00 +0800

    Update migration

--- src/migrations/m180106_000000_setting.php
+++ src/migrations/m180106_000000_setting.php
@@ -17,15 +17,19 @@ class m180106_000000_setting extends Migration
             'name' => $this->string(255)->notNull()->unique()->defaultValue(''),
             'group' => $this->string(32)->notNull()->defaultValue(''),
             'type' => $this->string(32)->notNull()->defaultValue('string'),
-            'data' => $this->text()->null(),
-            'default_data' => $this->text()->null(),
-            'rule_name' => $this->string(255)->null()->defaultValue(''),
-            'rule_data' => $this->binary()->null(),
+            'description' => $this->string(255)->null(),
+            'value' => $this->text()->null(),
+            'default_value' => $this->text()->null(),
+            'definition_name' => $this->string(255)->null()->defaultValue(''),
+            'definition_data' => $this->binary()->null(),
             'sort_order' => $this->integer(11)->notNull()->defaultValue(50),
             'autoload' => $this->boolean()->null()->defaultValue(0),
             'created_at' => $this->integer(11)->null(),
             'updated_at' => $this->integer(11)->null(),
         ]);
+
+        $this->createIndex('name_index', '{{%setting}}', ['name']);
+        $this->createIndex('full_name_unique', '{{%setting}}', ['name', 'group'], true);
     }
 
     /**
