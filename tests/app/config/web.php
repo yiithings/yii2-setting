1ebbf754795da36e2ec56c2d0ffcd9778d3f47a1
panlatent <panlatent@gmail.com>
2018-01-09 09:51:13 +0800

    Update pretty url

--- tests/app/config/web.php
+++ tests/app/config/web.php
@@ -6,6 +6,11 @@ return [
         'request' => [
             'cookieValidationKey' => '4d3941409c099ef9598dd7f893bb8d30702cc33ed015aa0b8fe7b4af8fc5b8d0',
         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => false,
+            'showScriptName' => false,
+        ]
     ],
     'on request' => function() {
 
